fe9eaaafa55a7c5b4f7f37b953256fcc10326705
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-29 21:21:04 +0100

    add userId slice, mutex lock and  joinservice method

--- Server/Server.go
+++ Server/Server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"strconv"
+	"sync"
 
 	h "../HelperMethod"
 	"google.golang.org/grpc"
@@ -18,16 +20,56 @@ const (
 
 type Server struct {
 	Proto.UnimplementedProtoServiceServer
-	port int
+	port           int
+	latestValue    Value
+	connectedUsers []int
+	arbiter        sync.Mutex
+}
+
+type Value struct {
+	value  int64
+	userId int64
 }
 
 func main() {
 
 	//init
-	server := Server{port: SERVER_PORT}
+	initValue := Value{value: -1, userId: -1}
+	users := make([]int, 0)
+	lock := sync.Mutex{}
+	server := Server{port: SERVER_PORT, latestValue: initValue, connectedUsers: users, arbiter: lock}
 
 	listen(&server)
 	fmt.Println("main has ended")
+}
+
+// JoinService grpc method logic
+func (s *Server) JoinService(ctx context.Context, request *Proto.JoinRequest) (*Proto.Response, error) {
+	s.arbiter.Lock()
+	var msg string
+	userId := int(request.GetUserId())
+
+	// check if user id already exist in the array
+	for _, user := range s.connectedUsers {
+		if user == userId {
+			msg = fmt.Sprintf("A user with id: %v has already joined", userId)
+			break
+		}
+	}
+	
+	//add userid to slice
+	if msg == "" {
+		s.connectedUsers = append(s.connectedUsers, userId)
+		msg = fmt.Sprintf("Welcome user: %v", userId)			
+	}
+	s.arbiter.Unlock()
+	return &Proto.Response{Msg: msg}, nil
+}
+
+// getvalue grpc method logic
+func (s *Server) GetValue(ctx context.Context, request *Proto.GetRequest) (*Proto.Value, error) {
+	value := Proto.Value{CurrentValue: s.latestValue.value, UserId: s.latestValue.userId}
+	return &value, nil
 }
 
 // start server service
@@ -42,5 +84,5 @@ func listen(s *Server) {
 	grpcServer := grpc.NewServer()
 	Proto.RegisterProtoServiceServer(grpcServer, s)
 	errorMsg := grpcServer.Serve(lis)
-	h.CheckError(errorMsg, "server listen registerservice")
+	h.CheckError(errorMsg, "server listen register server service")
 }
