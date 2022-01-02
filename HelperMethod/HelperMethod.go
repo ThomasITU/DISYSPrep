23b88115ada72a71f82fab26e6cd3e47320f04e1
Thomas Wolgast Rørbech <thwr@itu.dk>
2022-01-02 18:55:09 +0100

    add loggerWithTimestamp
    for formatting the log

--- HelperMethod/HelperMethod.go
+++ HelperMethod/HelperMethod.go
@@ -14,20 +14,31 @@ func CheckError(err error, msg string) {
 
 // log message in file
 func Logger(message string, logFileName string) {
-	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
-	defer f.Close()
+	defer file.Close()
 
-	log.SetOutput(f)
+	log.SetOutput(file)
 	log.Println(message)
 }
 
 // for lamport time stamp
-func Max(clientTimeStamp int64, serverTimeStamp int64) int64 {
+func Max(clientTimeStamp int64, serverTimeStamp int64) int64{
+	
 	if clientTimeStamp <= serverTimeStamp {
 		return serverTimeStamp
-	}
+	}  
 	return clientTimeStamp
 }
+	
+
+
+// for formating easier to verify
+func LoggerWithTimestamp(message string, logFileName string, timestamp int64) {
+	msg := fmt.Sprintf("Timestamp(%v): %v", timestamp, message) 
+	Logger(msg,logFileName)
+}
+	
+
